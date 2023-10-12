465a3099c0d59256abf7c1e74eedf76d1a399828
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-10-12 21:19:55 +0100

    check (fix): book path; add .yamllint

--- check.sh
+++ check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yamllint manifest.yaml
-../admin-tools-legacy/gce-develop/bin/book manifest check manifest.yaml
+# relies on admin-tools repo being in parent dir
+../admin-tools/ed0/bin/book manifest check manifest.yaml
 c=$(cat manifest.yaml | grep '}}}' | wc -l)
 if ! [ $c -eq 0 ]; then
     echo "too many curly braces - app may not work!"
