ae937ef2bc2e8485342d9912a85f57a80642d85c
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-10-12 21:24:19 +0100

    scripts (fix): server details

--- export.sh
+++ export.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export BOOK_CLIENT_SECRET=$(cat ~/secret/v0/book.pat)
+export BOOK_CLIENT_SECRET=$(cat ~/secret/app.practable.io/ed0/book.pat)
 export BOOK_CLIENT_TOKEN_TTL=5m
 export BOOK_CLIENT_TOKEN_ADMIN=true
-export BOOK_CLIENT_TOKEN_AUD=https://dev.practable.io/book
+export BOOK_CLIENT_TOKEN_AUD=https://app.practable.io/ed0/book
 export BOOK_CLIENT_TOKEN_SUB=admin
 
-export BOOK_CLIENT_TOKEN=$(book token)
+export BOOK_CLIENT_TOKEN=$(../admin-tools/ed0/bin/book token)
 export BOOK_CLIENT_SCHEME=https
-export BOOK_CLIENT_HOST=dev.practable.io
-export BOOK_CLIENT_BASE_PATH=/book/api/v1
+export BOOK_CLIENT_HOST=app.practable.io
+export BOOK_CLIENT_BASE_PATH=/ed0/book/api/v1
 export BOOK_CLIENT_FORMAT=YAML
 
-book manifest export
+../admin-tools/ed0/bin/book manifest export
 
